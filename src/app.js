875eb8cc484be57b61dfc3ed5e1636f9b73b93aa
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-05-18 19:39:42 -0300

    sucrase, eslint, prettier e edit config

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlerares();
+    this.routes();
+  }
+
+  middlerares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
