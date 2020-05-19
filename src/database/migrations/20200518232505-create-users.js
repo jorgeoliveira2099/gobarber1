c52a005ab32b896791bb6cd0d54fbf2a9b36c74d
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-05-18 23:25:11 -0300

    loader de usuários

--- src/database/migrations/20200518232505-create-users.js
+++ src/database/migrations/20200518232505-create-users.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable('user', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -25,7 +25,7 @@ module.exports = {
         defaultValue: false,
         allowNull: false,
       },
-      create_at: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
@@ -37,6 +37,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('user');
   },
 };
